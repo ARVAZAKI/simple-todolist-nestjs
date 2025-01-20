1e68e39258e4aceeb046bcc04f34019c4088434a
ARVAZAKI <arvazf09@gmail.com>
2025-01-20 17:57:54 +0700

    adding crud todo-list api

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Param, Delete } from '@nestjs/common';
+
+import { TodoService } from './todo.service';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+      constructor(private service: TodoService) {}
+
+      @Get()
+      async getTodos(): Promise<any> {
+            var todo = await this.service.getTodos();
+            return {
+                  message: "berhasil mendapatkan data todo-list", 
+                  data: todo
+            };
+      }
+
+      @Post()
+      async createTodo(@Body('title') title: string){
+            var todo = await this.service.createTodo(title);
+            return {
+                  message: "berhasil membuat todo-list", 
+                  data: todo
+            };
+      }
+
+      @Delete(':id')
+      async deleteTodo(@Param('id') id: string){
+            var todo = await this.service.deleteTodo(Number(id));
+            return{
+                  message: "berhasil menghapus todo-list",
+                  data: todo
+            }
+      }
+
+      @Put('/complete/:id')
+      async complete(@Param('id') id: string){
+            var todo = await this.service.completeTodo(Number(id))
+            return{
+                  message: "berhasil menyelesaikan task",
+                  data: todo
+            }
+      }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+      constructor(private prismaService: PrismaService) {}
+
+      getTodos(){
+            return this.prismaService.todo.findMany();
+      }
+
+      createTodo(title: string){
+            return this.prismaService.todo.create({
+                  data: {
+                        title: title,
+                        completed: false
+                  }
+            })
+      }
+
+      deleteTodo(id: number){
+            return this.prismaService.todo.delete({
+                  where: {
+                        id: id
+                  }
+            })
+      }
+
+      completeTodo(id: number){
+            return this.prismaService.todo.update({
+                  where: {
+                        id: id
+                  }, 
+                  data:{
+                        completed: true
+                  }
+            })
+      }
+}
